7bfe5a091d4e330713f25aa126ad2cc9030b329f
Haggerty <dhaggerty@pronet.com>
2018-06-05 08:49:42 -0500

    cloned from dataView version. We will diverge from here and make a datagrid version

---
+++ photolog/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace photolog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonOpenFolder = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.publishButton = new System.Windows.Forms.Button();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.clearTextButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 36);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(182, 591);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(283, 7);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(90, 23);
+            this.ClearButton.TabIndex = 3;
+            this.ClearButton.Text = "Clear All Photos";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(283, 36);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(140, 591);
+            this.listView2.TabIndex = 4;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // buttonOpenFolder
+            // 
+            this.buttonOpenFolder.Location = new System.Drawing.Point(12, 7);
+            this.buttonOpenFolder.Name = "buttonOpenFolder";
+            this.buttonOpenFolder.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFolder.TabIndex = 5;
+            this.buttonOpenFolder.Text = "Open Folder";
+            this.buttonOpenFolder.UseVisualStyleBackColor = true;
+            this.buttonOpenFolder.Click += new System.EventHandler(this.buttonOpenFolder_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(208, 36);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(59, 23);
+            this.addButton.TabIndex = 6;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(208, 75);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(59, 23);
+            this.removeButton.TabIndex = 7;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // publishButton
+            // 
+            this.publishButton.Location = new System.Drawing.Point(209, 177);
+            this.publishButton.Name = "publishButton";
+            this.publishButton.Size = new System.Drawing.Size(58, 23);
+            this.publishButton.TabIndex = 8;
+            this.publishButton.Text = "Publish";
+            this.publishButton.UseVisualStyleBackColor = true;
+            // 
+            // listView3
+            // 
+            this.listView3.Location = new System.Drawing.Point(729, 36);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(202, 591);
+            this.listView3.TabIndex = 9;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            // 
+            // clearTextButton
+            // 
+            this.clearTextButton.Location = new System.Drawing.Point(729, 7);
+            this.clearTextButton.Name = "clearTextButton";
+            this.clearTextButton.Size = new System.Drawing.Size(110, 23);
+            this.clearTextButton.TabIndex = 10;
+            this.clearTextButton.Text = "Clear All Text";
+            this.clearTextButton.UseVisualStyleBackColor = true;
+            this.clearTextButton.Click += new System.EventHandler(this.clearTextButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(430, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(293, 591);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 639);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.clearTextButton);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.publishButton);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.buttonOpenFolder);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonOpenFolder;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button publishButton;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.Button clearTextButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ photolog/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace photolog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // listView1 PROPERTIES... Details, List, Tiles
+            listView1.View = View.Details;
+            listView1.Columns.Add("listView1 Column1", 150);
+            listView1.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.HeaderSize);
+
+            // listView2 PROPERTIES... Details, List, Tiles
+            listView2.View = View.Details;
+            listView2.Columns.Add("listView2 Column1", 150);
+            listView2.Columns.Add("listView2 Column2", 300);
+            listView2.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.HeaderSize);
+
+            // listView2 PROPERTIES... Details, List, Tiles
+            listView3.View = View.Details;
+            listView3.Columns.Add("listView3 Column1", 300);
+            //listView2.Columns.Add("listView2 Column2", 200);
+            listView3.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            listView2.Items.Clear();
+        }
+
+        private void buttonOpenFolder_Click(object sender, EventArgs e)
+        {
+            // create instance of folderBrowserDialog class
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            // set root folder
+            // fbd.RootFolder = Environment.SpecialFolder.MyDocuments;
+            fbd.Description = "Choose a folder of pictures to upload";
+            // check user selects pass
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                // selected path most import property in folderBrowserDialog is SelectedPath
+                // MessageBox.Show(fbd.SelectedPath);
+
+            // clear previous list
+            listView1.Items.Clear();
+
+            // IMGLISTS TO HOLD IMAGES
+            ImageList imgList1 = new ImageList();
+            imgList1.ImageSize = new Size(100, 100);
+            listView1.SmallImageList = imgList1;
+            listView2.SmallImageList = imgList1;
+
+            //string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+            //foreach (string file in files)
+            //{
+            //    imgList1.Images.Add(Image.FromFile(file));
+            //    string fileName = Path.GetFileNameWithoutExtension(file);
+            //    int indexer;
+            //    int.TryParse(file, out indexer);
+            //    // change below '0' to each filename iteration
+            //    // listView1.Items.Add(fileName, 0);
+            //    listView1.Items.Add(fileName, indexer);
+            //    Console.WriteLine(indexer);
+            //}
+
+            string[] files = Directory.GetFiles(fbd.SelectedPath);
+            for (int i = 0; i < files.Length; i++)
+            {
+                imgList1.Images.Add(System.Drawing.Image.FromFile(files[i]));
+                string fileName = Path.GetFileNameWithoutExtension(files[i]);
+                //listView1.Items.Add(files[i], i);
+                listView1.Items.Add(fileName, i);
+            }
+        }
+
+        // Move selected items from one List to another.
+        private void CopySelectedItems(ListView source, ListView target)
+        {
+            foreach (ListViewItem item in source.SelectedItems)
+            {
+                target.Items.Add((ListViewItem)item.Clone());
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            CopySelectedItems(listView1, listView2);
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem eachItem in listView2.SelectedItems)
+            {
+                listView2.Items.Remove(eachItem);
+            }
+        }
+
+        private void clearTextButton_Click(object sender, EventArgs e)
+        {
+            listView3.Items.Clear();
+        }
+    }
+}
+
