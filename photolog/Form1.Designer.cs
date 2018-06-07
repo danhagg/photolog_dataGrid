6da6771ee779da6763b965d09be209f084ee60cd
Haggerty <dhaggerty@pronet.com>
2018-06-07 16:39:01 -0500

    more word output

--- photolog/Form1.Designer.cs
+++ photolog/Form1.Designer.cs
@@ -33,10 +33,13 @@
             this.buttonOpenFolder = new System.Windows.Forms.Button();
             this.removeButton = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.columnImage = new System.Windows.Forms.DataGridViewImageColumn();
-            this.columnText = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.addDGButton = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.columnImage = new System.Windows.Forms.DataGridViewImageColumn();
+            this.columnText = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FilePath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -74,22 +77,12 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.columnImage,
-            this.columnText});
+            this.columnText,
+            this.FilePath});
             this.dataGridView1.Location = new System.Drawing.Point(372, 36);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(657, 591);
             this.dataGridView1.TabIndex = 11;
-            // 
-            // columnImage
-            // 
-            this.columnImage.HeaderText = "Image";
-            this.columnImage.Name = "columnImage";
-            // 
-            // columnText
-            // 
-            this.columnText.HeaderText = "Text";
-            this.columnText.Name = "columnText";
-            this.columnText.Width = 500;
             // 
             // addDGButton
             // 
@@ -111,11 +104,52 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // columnImage
+            // 
+            this.columnImage.HeaderText = "Image";
+            this.columnImage.Name = "columnImage";
+            // 
+            // columnText
+            // 
+            this.columnText.HeaderText = "Text";
+            this.columnText.Name = "columnText";
+            this.columnText.Width = 500;
+            // 
+            // FilePath
+            // 
+            this.FilePath.HeaderText = "FilePath";
+            this.FilePath.Name = "FilePath";
+            this.FilePath.ReadOnly = true;
+            this.FilePath.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(291, 251);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(291, 281);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "This";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1041, 639);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.addDGButton);
             this.Controls.Add(this.dataGridView1);
@@ -138,9 +172,12 @@
         private System.Windows.Forms.Button removeButton;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button addDGButton;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridViewImageColumn columnImage;
         private System.Windows.Forms.DataGridViewTextBoxColumn columnText;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FilePath;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
